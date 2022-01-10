a67b2bafd6ae8db4e8eb84a816905c5c34c1e09a
wangzhiyun <wangzhiyun@sina.com>
2022-01-10 20:39:45 +0800

    add header component and link it to home and shop page

--- src/App.js
+++ src/App.js
@@ -3,46 +3,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Route, Routes } from "react-router";
-import { Link, useLocation } from 'react-router-dom';
+import Header from './components/header/header.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const HomePageDemo = (props) => {
-  console.log(props);
-  const location = useLocation();
-  console.log(location);
-  return <div>
-    <Link to='/topics'>TOPICS</Link>
-    <h1>HOME PAGE</h1>
-  </div>
-};
-const TopicListPage = () => (
-  <div>
-    TOPIC LIST PAGE
-  </div>
-);
-const TopicDetailPage = () => (
-  <div>
-    TOPIC DETAIL PAGE
-  </div>
-);
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route exact path='/' element={<HomePage/>}/>
         <Route path='/shop' element={<ShopPage/>} />
       </Routes>
-      {/* <Routes>
-        <Route exact path="/" element={<HomePageDemo/>} />
-        <Route exact path="/topics" element={<TopicListPage/>} />
-        <Route exact path="/topics/:topicId" element={<TopicDetailPage/>} />
-      </Routes> */}
     </div>
   );
 }
