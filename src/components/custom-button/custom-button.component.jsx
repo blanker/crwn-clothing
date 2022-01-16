69a27fe1c6ebad1bd3d85c03c24c3aa2f623d754
wangzhiyun <wangzhiyun@sina.com>
2022-01-16 15:13:14 +0800

    updated some styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './custom-button.styles.scss';
+import { CustomButtonContainer } from "./custom-button.styles";
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
     <button
@@ -12,5 +13,9 @@ const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => 
         {children}
     </button>
 );
+
+// const CustomButton = ({children, ...props}) => (
+//     <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+// );
 
 export default CustomButton;
